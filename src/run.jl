4c537113eb723aba9cbae45f063f35e5ab927c78
Sunoru <s@sunoru.com>
2023-09-15 00:13:26 -0400

    Add inplace buffers.

--- src/run.jl
+++ src/run.jl
@@ -3,7 +3,7 @@ using MosimoBase.LinearAlgebra: norm_sqr
 function default_callback(setup::GDSetup)
     logging_period = max(10, setup.max_steps ÷ 20)
     final = original_positions(setup.final)
-    @printf  " Step |  Distance  |  Length\n"
+    @printf " Step |  Distance  |  Length\n"
     (state::GDState; force_logging::Bool = false) -> begin
         if force_logging || state.step % logging_period ≡ 0
             @printf "%5d | %9.5f  | %8.5f\n" state.step √(state.dist²) state.path_length
@@ -17,46 +17,54 @@ distance(r1, r2) = √(distance2(r1, r2))
 distance(s1::MosiSystem, s2::MosiSystem) = distance(original_positions(s1), original_positions(s2))
 
 function move!(
-    rs, direction, model, E_L, δR
+    rs, direction, model, E_L, δR,
+    inplace_∇C
 )
-    ∇C = constraint_gradients(model, rs)
+    ∇C = constraint_gradients(
+        model, rs;
+        inplace=@view(inplace_∇C[:, 1:end-1]),
+        norm=true
+    )
     V = potential_energy_function(model, rs)
     projector = if V > E_L
-        ∇V = potential_energy_gradients(model, rs)
-        projection_matrix([∇C..., ∇V])
+        ∇V = potential_energy_gradients(model, rs; inplace=@view(inplace_∇C[:, end]), norm=true)
+        projection_matrix(inplace_∇C, normed=true)
     else
-        projection_matrix(∇C)
+        projection_matrix(∇C, normed=true)
     end
     T = eltype(direction)
-    new_direction = unflatten(T, projector * flatten(direction))
+    new_direction = projector * direction
     rs .+= δR * normalize(new_direction)
 end
 
-function constraint_corrector(C, ∇C)
+# ∇C should not be normalized.
+function constraint_corrector(
+    C, ∇C,
+)
     N_C = length(C)
     c = zeros(Float64, N_C)
-    ∇C_unit_new = normalize.(∇C)
-    for i in 1:N_C
-        ∇Cᵢ_unit = copy(∇C_unit_new[i])
-        F = Float64(1.0)
-        c[i] = C[i] / norm(∇C[i])
+    ∇C_unit_new = normalize_columns(∇C)
+    @inbounds for i in 1:N_C
+        ∇Cᵢ_unit = copy(∇C_unit_new[:, i])
+        F = 1.0
+        c[i] = C[i] / norm(∇C[:, i])
         for j = 1:i-1
-            γⱼᵢ = ∇C_unit_new[j] ⋅ ∇Cᵢ_unit
-            F -= γⱼᵢ ^ 2
+            γⱼᵢ = ∇C_unit_new[:, j] ⋅ ∇Cᵢ_unit
+            F -= γⱼᵢ^2
             c[i] += c[j] * γⱼᵢ
-            ∇C_unit_new[i] -= ∇C_unit_new[j] * γⱼᵢ
+            ∇C_unit_new[:, i] -= ∇C_unit_new[:, j] * γⱼᵢ
         end
-        ∇C_unit_new[i] /= √F
+        ∇C_unit_new[:, i] /= √F
         c[i] /= -√F
     end
-    sum(c .* ∇C_unit_new)
+    sum(∇C_unit_new .* c', dims=2)
 end
 
-function constraint_corrections!(rs, model, E_L)
+function constraint_corrections!(rs, model, E_L, inplace_C, inplace_∇C)
     N = length(rs)
     E_Lt = E_L + abs(E_L) * 1e-6
     δC = N * 1e-12
-    C = constraints(model, rs)
+    C = constraints(model, rs; inplace=@view(inplace_C[1:end-1]))
     V = potential_energy_function(model, rs)
     constraint_satisfied = sum(C .^ 2) ≤ δC
     potential_satisfied = V ≤ E_Lt
@@ -66,14 +74,19 @@ function constraint_corrections!(rs, model, E_L)
         if any(isnan.(C)) || isnan(V)
             error("Something went wrong in the correction.")
         end
-        ∇C = constraint_gradients(model, rs)
+        ∇C = constraint_gradients(
+            model, rs;
+            inplace=@view(inplace_∇C[:, 1:end-1]),
+            norm=false
+        )
         if !potential_satisfied
-            push!(C, V - E_L)
-            ∇V = potential_energy_gradients(model, rs)
-            push!(∇C, ∇V)
+            inplace_C[end] = V - E_L
+            C = inplace_C
+            ∇C = inplace_∇C
+            ∇V = potential_energy_gradients(model, rs; inplace=@view(inplace_∇C[:, end]), norm=false)
         end
         rs .+= constraint_corrector(C, ∇C)
-        C = constraints(model, rs)
+        C = constraints(model, rs; inplace=@view(inplace_C[1:end-1]))
         V = potential_energy_function(model, rs)
         constraint_satisfied = sum(C .^ 2) ≤ δC
         potential_satisfied = V ≤ E_Lt
@@ -83,9 +96,9 @@ end
 
 function gd_run(
     setup::GDSetup,
-    callback::Nullable{F} = nothing;
-    return_result = true,
-) where {F <: Function}
+    callback::Nullable{F}=nothing;
+    return_result=true
+) where {F<:Function}
     model = setup.model
     δR = setup.δR
     max_steps = setup.max_steps
@@ -104,23 +117,31 @@ function gd_run(
     @printf "E_L: %8.5f\n" E_L
     @printf "Distance: %8.5f\n" distance(current, final)
 
+    just_moved = zero_similar(current)
+    # Only use one C or ∇C buffer and give them one extra column.
+    inplace_C = constraints(model, current)
+    push!(inplace_C, 0.0)
+    inplace_∇C = constraint_gradients(model, current)
+    inplace_∇C = [inplace_∇C zero_similar(current)]
     next_direction = callback(state)
     state.dist² = norm_sqr(next_direction)
     while true
-        if state.dist² < δR ^ 2 || state.step ≥ max_steps
+        if state.dist² < δR^2 || state.step ≥ max_steps
             break
         end
         previous = copy(current)
         move!(
-            current, next_direction, model, E_L, δR
+            current, next_direction, model, E_L, δR,
+            inplace_∇C
         )
-        constraint_corrections!(current, model, E_L)
+        constraint_corrections!(current, model, E_L, inplace_C, inplace_∇C)
         state.step += 1
-        state.path_length += distance(current, previous)
+        just_moved .= current .- previous
+        state.path_length += norm(just_moved)
         next_direction = callback(state)
         state.dist² = norm_sqr(next_direction)
     end
-    callback(state; force_logging = true)
+    callback(state; force_logging=true)
     @printf "GD finished.\n"
 
     result = return_result ? GDResult(state.step, state.path_length, state.configuration) : state.path_length
